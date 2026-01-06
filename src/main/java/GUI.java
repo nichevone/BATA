28e529e7c7c61c43d173860eb554c7eb7644b53f
nichevone <stepan.mikhailov.20@gmail.com>
2026-01-06 22:20:41 +1000

    Commit for pushing on repo. Some Maven building troubles solved. A little GUI improvments

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,85 +1,125 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.*;
+import java.awt.*;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class GUI implements ActionListener {
-    private final int WINDOW_WIDTH = 600;
-    private final int WINDOW_HEIGHT = 400;
+    private final int windowWidth;
+    private final int windowHeight;
 
     private final String HOST_CARD = "HOST";
     private final String CONNECT_CARD = "CONNECT";
-    private final String MAIN_CARD = "MAIN";
+    private final String START_CARD = "START";
 
-    private final JFrame frame = new JFrame();
-    private JPanel cards;
+    private final Font titleLabelFont = new Font("SansSerif", Font.BOLD, 32);
+    private final Font textLabelFont = new Font("SansSerif", Font.PLAIN, 16);
+    private final Font buttonFont = new Font("SansSerif", Font.ITALIC, 24);
+
+    private JFrame frame;
+    private JPanel cardsPanel;
     private CardLayout cardLayout;
 
-    public GUI() {
+    private JPanel startCard;
+    private JPanel hostingCard;
+    private JPanel connectingCard;
+    private JButton hostButton;
+    private JButton connectButton;
+
+    public GUI(int windowWidth, int windowHeight) {
+        this.windowWidth = windowWidth;
+        this.windowHeight = windowHeight;
         initUI();
     }
 
     private void initUI() {
+        frame = new JFrame();
         cardLayout = new CardLayout();
-        cards = new JPanel(cardLayout);
+        cardsPanel = new JPanel(cardLayout);
 
-        // Main window for choosing action
-        JPanel actionChoice = new JPanel();
-        JPanel startCard = new JPanel();
+        startCard = createStartCard();
+        hostingCard = createHostCard();
+        connectingCard = createConnectCard();
 
-        JButton hostButton = new JButton("HOST");
-        JButton connectButton = new JButton("CONNECT");
-        hostButton.addActionListener(this);
-        connectButton.addActionListener(this);
-
-        actionChoice.add(hostButton);
-        actionChoice.add(connectButton);
-
-        // Main card
-        startCard.add(new JLabel("Choose action:", JLabel.CENTER), BorderLayout.CENTER);
-        startCard.add(actionChoice, BorderLayout.CENTER);
-
-        // Hosting window
-        JPanel hostingCard = new JPanel();
-        hostingCard.add(new JLabel("HOSTING"));
-
-        JButton returnButtonHost = new JButton("MAIN");
-        returnButtonHost.addActionListener(this);
-        hostingCard.add(returnButtonHost);
-
-        // Connecting window
-        JPanel connectingCard = new JPanel();
-        connectingCard.add(new JLabel("CONNECTING"));
-
-        JButton returnButtonConnect = new JButton("MAIN");
-        returnButtonConnect.addActionListener(this);
-        connectingCard.add(returnButtonConnect);
-
-        cards.add(hostingCard, HOST_CARD);
-        cards.add(connectingCard, CONNECT_CARD);
-        cards.add(startCard, MAIN_CARD);
-
-        // Adding panels to the frame
-        frame.add(cards, BorderLayout.CENTER);
+        cardsPanel.add(hostingCard, HOST_CARD);
+        cardsPanel.add(connectingCard, CONNECT_CARD);
+        cardsPanel.add(startCard, START_CARD);
+        frame.add(cardsPanel, BorderLayout.CENTER);
 
         frame.setTitle("BATA");
-        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.setIconImage(getIconImage());
+        frame.setSize(windowWidth, windowHeight);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
-        cardLayout.show(cards, MAIN_CARD);
+        cardLayout.show(cardsPanel, START_CARD);
+    }
+
+    private JPanel createStartCard() {
+        JPanel panel = new JPanel(new GridLayout(2, 1));
+        JPanel actionChoicePanel = new JPanel(new GridLayout(2, 1, 20, 20));
+
+        JLabel chooseActionLabel = new JLabel("Choose action:", JLabel.CENTER);
+        chooseActionLabel.setFont(titleLabelFont);
+
+        hostButton = new JButton(HOST_CARD);
+        hostButton.setFont(buttonFont);
+        hostButton.addActionListener(this);
+        connectButton = new JButton(CONNECT_CARD);
+        connectButton.setFont(buttonFont);
+        connectButton.addActionListener(this);
+
+        actionChoicePanel.add(hostButton);
+        actionChoicePanel.add(connectButton);
+
+        panel.add(chooseActionLabel, BorderLayout.NORTH); // Label first
+        panel.add(actionChoicePanel, BorderLayout.CENTER); // Panel w/ buttons after the label
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        return panel;
+    }
+
+    private JPanel createHostCard() {
+        JPanel hostPanel = new JPanel();
+        JLabel titleLabel = new JLabel("HOSTING");
+        titleLabel.setFont(textLabelFont);
+        hostPanel.add(titleLabel);
+
+        return hostPanel;
+    }
+
+    private JPanel createConnectCard() {
+        JPanel connectPanel = new JPanel();
+        JLabel titleLabel = new JLabel("CONNECTING");
+        titleLabel.setFont(textLabelFont);
+        connectPanel.add(titleLabel);
+
+        return connectPanel;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String command = e.getActionCommand();
         System.out.println(command);
-        cardLayout.show(cards, command);
+        cardLayout.show(cardsPanel, command);
+    }
+
+    private Image getIconImage() {
+        try {
+            return ImageIO.read(new File("src/main/resources/icon.png"));
+        } catch (IOException e) {
+            System.err.println("Error reading icon image.\n"+e.getMessage());
+        }
+        return null;
     }
 
     public static void main(String[] args) {
+        int width = 600, height = 400;
+
         EventQueue.invokeLater(() -> {
-            GUI gui = new GUI();
+            GUI gui = new GUI(width, height);
         });
     }
 }

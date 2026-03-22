baf7a4ffbc3e330ec24d2e117639e006a4c6e427
nichevone <stepan.mikhailov.20@gmail.com>
2026-03-22 15:19:54 +1000

    Create package_creators folder, edit windows.bat slightly / Rename ImageFiles folder to just images

---
+++ package_creators/windows.bat
@@ -0,0 +1,21 @@
+set "dd=%DATE:~0,2%"
+
+set "ts=%TIME: =0%"
+set "hh=%ts:~0,2%"
+set "min=%ts:~3,2%"
+
+set "TIME=%dd%.%hh%.%min%
+echo %TIME%
+
+set "JAR_PATH=../out/artifacts/VoiceChat_jar"
+set "JAR_NAME=VoiceChat_v1.1.0-beta"
+
+jpackage --app-version %TIME% ^
+--name %JAR_NAME% ^
+--input %JAR_PATH% ^
+--main-jar %JAR_NAME%.jar ^
+--type exe ^
+--win-dir-chooser ^
+--icon %JAR_PATH%/icon.ico
+
+pause
